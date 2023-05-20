4ef7d364a4a404a6761f1ace31e01ea5409401eb
Aaditya Murthy <aadi.murthyananth@gmail.com>
2023-05-20 15:02:15 -0500

    Initial Commit

---
+++ src/main/java/com/amurthy/tools/App.java
@@ -0,0 +1,13 @@
+package com.amurthy.tools;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
